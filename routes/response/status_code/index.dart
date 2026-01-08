8054bed3872b058af232c474a02adfafa130b35f
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-08 18:14:50 +0900

    Status Code

---
+++ routes/response/status_code/index.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  return Response(
+    statusCode: HttpStatus.unauthorized,
+    body: 'Unauthorized',
+  );
+}
