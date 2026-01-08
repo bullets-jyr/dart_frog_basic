f154d90813692a1df11aa69647077cb5f7df07d8
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-08 16:42:41 +0900

    Request Body - String

---
+++ routes/request/body/string/index.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final method = context.request.method;
+
+  switch (method) {
+    case HttpMethod.post:
+      final body = await context.request.body();
+      return Response(
+        body: 'bodyType: ${body.runtimeType}, content: $body',
+      );
+    case _:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
